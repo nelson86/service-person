a3b819395997cbcf0ac4419268fb66698a8febfd
nelson <nelson.arabel@gmail.com>
2020-09-29 22:52:23 -0300

    Integracion de Swagger

---
+++ src/main/java/com/narabel/service/person/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.narabel.service.person.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * http://localhost:8001/swagger-ui.html
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(
+                        RequestHandlerSelectors
+                                .basePackage("com.narabel.service.person.controllers"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
